9d4023a2c541e3f286c719ca4a0998852167d2b8
ThomasBavn <102732812+ThomasBavn@users.noreply.github.com>
2022-09-02 13:32:01 +0200

    print hello world (#2)
    * empty writeline
    
    * Prints "Hello, World!"

--- Assignment_0.Tests/ProgramTests.cs
+++ Assignment_0.Tests/ProgramTests.cs
@@ -3,8 +3,19 @@ namespace Assignment_0.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Main_()
+    public void Main_Should_Print_Hello_World()
     {
-       
+// Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+         // Act
+        // var program = Assembly.Load(nameof(Program));
+        Program.Main(Array.Empty<string>());
+        // program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Hello, World!");       
     }
 }
--- Assignment_0/Program.cs
+++ Assignment_0/Program.cs
@@ -1,8 +1,9 @@
-﻿namespace Assignment_0;
-
+﻿
 public class Program
 {
-    private static void main(string[] args){
-        Console.WriteLine("");
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, World!");
     }
 }
